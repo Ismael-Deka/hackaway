957c35d0adacb0d836696b74a2c776f3bf59a71b
Ismael-Deka <ismael.deka7899@gmail.com>
2023-09-24 16:31:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hackaway
+
+## Getting Started
+
+"npm install"
+"npm run build"
+"python manage.py runserver"
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        
+      </div>
+      <h1>Hackaway</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
